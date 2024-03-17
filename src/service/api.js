93c4997542368f4b6aa7ab3dee8a85f4b3614aaf
MD SHAMIM AKHTER <contactshamim62@gmail.com>
2024-03-17 20:30:37 +0530

    security added

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_URL = process.env.REACT_APP_API_URL
 const API_KEY = process.env.REACT_APP_API_KEY;
 export const fetchData= async(city, country)=>{
     try {
